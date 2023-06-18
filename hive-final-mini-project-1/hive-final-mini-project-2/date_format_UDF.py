58f9b4ab67b28ab0b128605e513efb099e6e298a
Dinesh Boliwar <117983166+dinesh-5678@users.noreply.github.com>
2023-06-18 19:16:46 +0530

    Delete hive-final-mini-project-1/hive-final-mini-project-2 directory

--- hive-final-mini-project-1/hive-final-mini-project-2/date_format_UDF.py
+++
@@ -1,8 +0,0 @@
-import sys
-from datetime import datetime
-
-for line in sys.stdin:
-    line = line.strip("\n\r")
-    date_old_format = str(line)
-    date_time_obj = datetime.strptime(date_old_format, '%d/%m/%Y')
-    print(date_time_obj.date())
